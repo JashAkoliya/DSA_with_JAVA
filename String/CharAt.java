ad7df8a24e0b6acc721b74a81f8c460cf8e22289
Akoliya Jash Vijaybhai <100468694+JashAkoliya@users.noreply.github.com>
2023-07-16 14:00:49 +0530

    Create CharAt.java

---
+++ String/CharAt.java
@@ -0,0 +1,11 @@
+package PracticeStart.String;
+
+public class CharAt {
+    public static void main(String[] args) {
+        String s = "Jash";
+
+        for(int i=0;i<s.length();i++){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
